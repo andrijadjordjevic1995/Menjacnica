9cb4fd55dae0b2bc9fe2e002b7bf74bced5c2133
andrijadjordjevic1995 <andrija.djordjevic1995@gmail.com>
2016-03-23 22:25:00 +0100

    Do kraja implementirane metode iz interfejsa InterfejsMenjacnica.

--- Menjacnica/src/menjacnica/ImplementacijaInterfejsa.java
+++ Menjacnica/src/menjacnica/ImplementacijaInterfejsa.java
@@ -1,6 +1,7 @@
 package menjacnica;
 
 import java.util.Date;
+import java.util.LinkedList;
 
 import paket_interfejs.InterfejsMenjacnica;
 
@@ -8,19 +9,37 @@ public class ImplementacijaInterfejsa implements InterfejsMenjacnica {
 
 	@Override
 	public void dodajKursValute(Valuta v, Date datum, double prodajni, double srednji, double kupovni) {
-		// TODO Auto-generated method stub
+		if(v == null && datum == null)
+			throw new RuntimeException();
+		
+		Kurs noviKurs = new Kurs();
+		noviKurs.setDatum(datum);
+		noviKurs.setKupovni(kupovni);
+		noviKurs.setSrednji(srednji);
+		noviKurs.setProdajni(prodajni);
+		v.getListaKurseva().add(noviKurs);
+			
 
 	}
 
 	@Override
 	public void brisiKursValute(Valuta v, Date datum) {
-		// TODO Auto-generated method stub
+		LinkedList<Kurs> lista = v.getListaKurseva();
+		for(int i = 0; i < lista.size(); i++){
+			if(lista.get(i).getDatum().equals(datum)){
+				lista.remove(i);
+			}
+		}
 
 	}
 
 	@Override
 	public Kurs pronadjiIVratiKurs(Valuta v, Date datum) {
-		// TODO Auto-generated method stub
+		LinkedList<Kurs> lista = v.getListaKurseva();
+		for (Kurs kurs : lista) {
+			if(kurs.getDatum().equals(datum))
+				return kurs;
+		}
 		return null;
 	}
 
